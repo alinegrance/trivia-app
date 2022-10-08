9ba00fbbc31f92c16532da5fe04398433ec2faf6
Eduardo Balieiro - turma 24 - tribo A <eduferabalfer56@gmail.com>
2022-10-07 21:40:22 -0300

    arrumando o Play pra Game

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Header from '../components/Header';
 
-class Play extends React.Component {
+class Game extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        Play
+        Game
       </div>
     );
   }
 }
 
-export default Play;
+export default Game;
